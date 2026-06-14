13785ef5e2e9591b834187373a05a337510b63e4
pajlyons10-create <pajlyons10@gmail.com>
2026-06-14 20:58:13 +0100

    Delete puppeteer.config.cjs

--- puppeteer.config.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Forces Puppeteer to install Chrome directly inside your project folder
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
